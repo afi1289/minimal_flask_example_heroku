e1adf9ebe0efd6c86b1b2a035718df749f39d04b
Ahmad Alismail <46696930+afi1289@users.noreply.github.com>
2022-06-08 10:36:35 +0200

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,27 +1,21 @@
-#def useless_function():
-#    """ 
-#    A function that returns some text
-#    """
-#    return("some text")
-
-
 from flashtext.keyword import KeywordProcessor
 import pickle
 
-# Function that takes loads in our pickled word processor
+# Funtion that takes loads in our pickeled word processor
 # and defines a function for using it. This makes it easy
 # to do these steps together when serving our model.
 def get_keywords_api():
     
     # read in pickled word processor. You could also load in
     # other models as this step.
-    keyword_processor = pickle.load(open("processor.pkl", "rb"))
+    with open("word_processor.pkl", "rb") as file:
+        keyword_processor = pickle.load(file)
     
-    # Function to apply our model & extract keywords from a 
+    # Function to apply our modle & extract keywords from a 
     # provided bit of text
-    def keywords_api(keywordProcessor, text, span_info=True): 
-        keywords_found = keywordProcessor.extract_keywords(text, span_info=True)      
+    def keywords_api(text): 
+        keywords_found = keyword_processor.extract_keywords(text, span_info=True)      
         return keywords_found
     
-    # return the function we just defined
+    # return the function
     return keywords_api
