d705985cd6d4298c02dab41a99f4d98fca42a616
Ahmad Alismail <46696930+afi1289@users.noreply.github.com>
2022-06-08 09:07:25 +0200

    Update script.py

--- script.py
+++ script.py
@@ -10,7 +10,7 @@
 
 import json
 from flask import Flask, request
-# from serve import get_keywords_api
+from serve import get_keywords_api
 
 # I've commented out the last import because it won't work in kernels, 
 # but you should uncomment it when we build our app tomorrow
