43ac802292e2c37cf131c61d4eb6811f3932b5b8
Ahmad Alismail <46696930+afi1289@users.noreply.github.com>
2022-06-08 09:42:20 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,44 +1,39 @@
-#from flask import Flask
-#from serve import useless_function  
-
-#app=Flask(__name__)
-
-## Define our "ping" end point
-#@app.route('/ping')
-#def useless_output():
-#  return(useless_function())
-
-import json
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from serve import get_keywords_api
+import json
 
-# I've commented out the last import because it won't work in kernels, 
-# but you should uncomment it when we build our app tomorrow
+app=Flask(__name__)
 
-# create an instance of Flask
-app = Flask(__name__)
+# This will print some text at our app's URL
+# so that we can check that it's working. If you 
+# wanted a more complex UI you could put it here.
+@app.route('/')
+def index():
+    return "Up and running!"
 
-# load our pre-trained model & function
+# load our pre-trained model & function to run it on
+# new data. This design is based on one proposed by 
+# Guillaume Genthial. More details: 
+#https://guillaumegenthial.github.io/serving.html
 keywords_api = get_keywords_api()
 
-# Define a post method for our API.
+# Define a post method for our API. 
 @app.route('/extractpackages', methods=['POST'])
+# function that this method will run
 def extractpackages():
-    """ 
-    Takes in a json file, extracts the keywords &
-    their indices and then returns them as a json file.
+    """Extract packages
+    Pass in a single json file with a text span in it. It 
+    will return any Python packages mentioned in that span,
+    along with their index (in characters).
     """
-    # the data the user input, in json format
-    input_data = request.json
-
-    # use our API function to get the keywords
+    # get the json file with our text data in it
+    input_data = request.get_json()
+    
+    # use our API function to extract the keywords
     output_data = keywords_api(input_data)
 
-    # convert our dictionary into a .json file
-    # (returning a dictionary wouldn't be very
-    # helpful for someone querying our API from
-    # java; JSON is more flexible/portable)
+    # convert our dictionary output into a .json file
     response = json.dumps(output_data)
-
+    
     # return our json file
     return response
